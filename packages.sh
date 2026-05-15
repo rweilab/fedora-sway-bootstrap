291ef7299ee5e72f02deb904822cdfe848a99fd2
Richard Wei <richardwei365@gmail.com>
2026-05-15 19:00:08 -0400

    feat(main): added some auxilary installer scrpts

--- packages.sh
+++ packages.sh
@@ -1,21 +1,29 @@
-echo "SLAP: Executing packages script"
+echo "SLAP: Executing @p"
 
+echo "SLAP: Installing COPR_PKGS"
 COPR_PKGS=(lihaohong/yazi, agriffis/neovim-nightly)
 for arg in ${COPR_PKGS[@]} ; do
   echo "SLAP: Enabling dnf copr package: $arg"
   dnf copr enable $arg
 done
 
+echo "SLAP: Installing PKGS"
 PKGS=(neovim yazi emacs tailscale syncthing)
 for arg in ${PKGS[@]} ; do
   echo "SLAP: Installing package: $arg"
   dnf install -y $arg
 done
 
+echo "SLAP: Installing BONUS_PKGS"
 BONUS_PKGS=(ripgrep fzf tealdeer ffmpeg fd-find)
 for arg in ${BONUS_PKGS[@]} ; do
   echo "SLAP: Installing package: $arg"
   dnf install -y $arg
 done
+
+
+echo "SLAP: Running auxilary installer scripts"
+./scripts/doom_emacs_installer.sh
+./scripts/protonvpn_installer.sh
 
 echo "SLAP: FINISHED running $0"
---
+++ scripts/protonvpn_installer.sh
@@ -0,0 +1,9 @@
+echo "SLAP: Executing $0"
+
+wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm"
+
+sudo dnf install -y ./protonvpn-stable-release-1.0.4-1.noarch.rpm && sudo dnf check-update --refresh 
+
+sudo dnf install -y proton-vpn-gnome-desktop 
+
+echo "SLAP: FINISHED running $0"
