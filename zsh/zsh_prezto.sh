3189b86a3867694ed56beb6d5fa58c0c4884849a
Richard Wei <richardwei365@gmail.com>
2026-05-15 13:31:44 -0400

    feat(prezdo): finalized zsh+prezdo bash script

---
+++ zsh/zsh_prezto.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo "SLAP: Installing dependencies for zsh+prezdo"
+sudo dnf install -y zsh git
+
+if [ ! -d "$HOME/.zprezto" ]; then
+  echo "SLAP: Cloning prezdo from github"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
+fi
+
+echo "SLAP: Running zpreszdo install commands"
+for rcfile in "$HOME/.zprezto/runcoms/"*; do
+  ln -sf "$rcfile" "$HOME/.$(basename "$rcfile")"
+done
