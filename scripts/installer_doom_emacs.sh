48e38b3aa4c1dd77bc5fde41b6fe6ce3592033ec
Richard Wei <richardwei365@gmail.com>
2026-05-16 12:15:20 -0400

    chore(doom): added a 'n' pipe to the doom install script

--- scripts/installer_doom_emacs.sh
+++ scripts/installer_doom_emacs.sh
@@ -1,6 +1,6 @@
 echo "SLAP: Executing @p"
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
+printf "n\n" | ~/.config/emacs/bin/doom install
 
 echo "SLAP: FINISHED running $0"
