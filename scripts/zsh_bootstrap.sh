e568e8e6fa26beb12e27d15c477bf70df07d1f48
Richard Wei <richardwei365@gmail.com>
2026-05-16 11:16:57 -0400

    fix(zsh): added XDG compliant redirect in /home/slap/.config/zsh/.zshenv

--- scripts/zsh_bootstrap.sh
+++ scripts/zsh_bootstrap.sh
@@ -10,6 +10,8 @@ echo "SLAP: Copying zsh files to XDG-compliant filepath"
 mkdir -p ~/.config/zsh/
 cp -t ~/.config/zsh/ "$SCRIPT_DIR/../zsh/user.zsh" 
 cp -t ~/.config/zsh/ "$SCRIPT_DIR/../zsh/plugin.zsh"
+cp "$SCRIPT_DIR/..zsh/xdg.zshenv" ~/.config/zsh/.zshenv
+
 cp -t ~/ "$SCRIPT_DIR/../zsh/.zshenv" 
 echo "SLAP: Complete"
 
