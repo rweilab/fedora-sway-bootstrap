f01603451e692e70f4a847de5bcc7234b1f4dbc9
Richard Wei <richardwei365@gmail.com>
2026-05-16 13:01:53 -0400

    chore(doom): changed n to y for doom installer script due to breakage

--- scripts/installer_doom_emacs.sh
+++ scripts/installer_doom_emacs.sh
@@ -1,6 +1,6 @@
 echo "SLAP: Executing @p"
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-printf "n\n" | ~/.config/emacs/bin/doom install
+printf "y\n" | ~/.config/emacs/bin/doom install
 
 echo "SLAP: FINISHED running $0"
