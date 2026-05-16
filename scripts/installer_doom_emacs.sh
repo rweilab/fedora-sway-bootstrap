36422fe2a872d94111a401cd59931e9e5e0ad4a7
Richard Wei <richardwei365@gmail.com>
2026-05-16 18:55:02 -0400

    fix(doom): adding if/else doom already exists

--- scripts/installer_doom_emacs.sh
+++ scripts/installer_doom_emacs.sh
@@ -4,7 +4,14 @@ SLAP_PRINT "Installing dependencies for doom emacs"
 sudo dnf install -y emacs git ripgrep
 SLAP_PRINT "Complete"
 
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
+EMACS_DIR="$HOME/.config/emacs"
+
+if [ -x "$EMACS_DIR/bin/doom" ]; then
+    echo "SLAP: Doom already installed, skipping"
+else
+    echo "SLAP: Installing Doom Emacs"
+    git clone --depth 1 https://github.com/doomemacs/doomemacs "$EMACS_DIR"
+    "$EMACS_DIR/bin/doom" install
+fi
 
 SLAP_PRINT "FINISHED running $0"
