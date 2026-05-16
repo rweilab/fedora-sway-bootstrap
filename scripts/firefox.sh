d92f44cf1be73871d287b5b6d0b9849880133263
Richard Wei <richardwei365@gmail.com>
2026-05-16 17:31:02 -0400

    temp push to test user.js

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "SLAP: Executing $0"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 firefox &
 PID=$!
@@ -8,3 +11,36 @@ while [ ! -d ~/.mozilla/firefox ]; do
 done
 
 kill $PID
+
+wget https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
+
+
+
+
+# ---
+echo "SLAP: Executing $0"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "SLAP: Killing Waybar"
+if pgrep waybar >/dev/null; then
+    pkill waybar
+fi
+
+mkdir -p ~/.config/waybar
+cp "$SCRIPT_DIR/../waybar/config.jsonc" ~/.config/waybar/
+cp "$SCRIPT_DIR/../waybar/style.css" ~/.config/waybar/
+echo "SLAP: Copied waybar files"
+
+mkdir -p ../waybar/fonts
+cd ../waybar/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+mkdir -p ~/.local/share/fonts/JetBrainsMono
+unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono
+rm JetBrainsMono.zip
+fc-cache -fv
+
+echo "SLAP: Reopening waybar"
+nohup waybar >/dev/null 2>&1 & disown
+
+echo "SLAP: FINISHED running $0"
