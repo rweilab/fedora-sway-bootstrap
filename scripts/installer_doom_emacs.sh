52d18aa09f351a21dd2eac562fde9f6c2aa3dfad
Richard Wei <richardwei365@gmail.com>
2026-05-16 13:22:01 -0400

    chore(doom): removed y/n pipe for doom installer due to errors

--- scripts/installer_doom_emacs.sh
+++ scripts/installer_doom_emacs.sh
@@ -1,6 +1,6 @@
 echo "SLAP: Executing @p"
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-printf "y\n" | ~/.config/emacs/bin/doom install
+~/.config/emacs/bin/doom install
 
 echo "SLAP: FINISHED running $0"
