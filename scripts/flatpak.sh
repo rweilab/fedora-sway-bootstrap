17a246ca371b9bca15e8fb62ada5887f488fed97
Richard Wei <richardwei365@gmail.com>
2026-05-16 21:30:56 -0400

    feat(flatpak): added flatpak, flathub, warehouse, utils..

---
+++ scripts/flatpak.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../lib.sh"
+SLAP_PRINT "Executing $0"
+
+commands=(
+  "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo"
+
+  "flatpak install -y flathub io.github.flattool.Warehouse"
+  "flatpak install -y flathub com.parsecgaming.parsec"
+  "flatpak install -y flathub org.onlyoffice.desktopeditors"
+  "flatpak install -y flathub com.vscodium.codium"
+  "flatpak install -y flathub md.obsidian.Obsidian"
+  "flatpak install -y flathub com.adilhanney.saber"
+)
+
+for cmd in "${commands[@]}"; do
+  SLAP_PRINT "→ $cmd"
+  eval "$cmd"
+done
+
+
+
+SLAP_PRINT "FINISHED running $0"
