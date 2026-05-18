e6ff3a483a8ea48f33e663ffcef52f1d3a636d4d
Richard Wei <richardwei365@gmail.com>
2026-05-17 23:57:56 -0400

    chore(vnc): added helper echo statements

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -13,6 +13,16 @@ sudo firewall-cmd --reload
 swaymsg create_output HEADLESS-1
 
 wayvnc --output=HEADLESS-1 --max-fps=20 0.0.0.0 5900 &
+
+echo -e "\033[1;33m=== WORKSPACES ===\033[0m"
+swaymsg -t get_workspaces
+
+echo
+echo -e "\033[1;33m=== IP ADDRESSES ===\033[0m"
+ip addr
+
+echo
+echo -e "\033[1;33mVNC should be available on port 5900\033[0m"
 EOF
 
 sudo chmod +x /usr/local/bin/start-vnc
