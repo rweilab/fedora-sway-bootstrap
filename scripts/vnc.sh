29d4a6921370df2a03290a5f91ba04625aa859fb
Richard Wei <richardwei365@gmail.com>
2026-05-17 22:47:49 -0400

    chore(vnc): fixing permission access

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -3,7 +3,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../lib.sh"
 SLAP_PRINT "Executing $0"
 
-cat > /usr/local/bin/start-vnc << 'EOF'
+sudo mkdir -p /usr/local/bin
+sudo tee /usr/local/bin/start-vnc.sh > /dev/null << 'EOF'
 #!/bin/bash
 
 sudo firewall-cmd --add-port=5900/tcp
@@ -14,7 +15,7 @@ swaymsg create_output HEADLESS-1
 wayvnc --output=HEADLESS-1 --max-fps=20 0.0.0.0 5900 &
 EOF
 
-chmod +x /usr/local/bin/start-vnc
+sudo chmod +x /usr/local/bin/start-vnc.sh
 
 echo "done"
 
