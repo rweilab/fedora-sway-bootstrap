7a35036ba7aee2748a0b8efe921ae83929108549
Richard Wei <richardwei365@gmail.com>
2026-05-17 22:56:49 -0400

    fix(vnc) removed .sh extension so zsh alias points to the right executable

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -4,7 +4,7 @@ source "$SCRIPT_DIR/../lib.sh"
 SLAP_PRINT "Executing $0"
 
 sudo mkdir -p /usr/local/bin
-sudo tee /usr/local/bin/start-vnc.sh > /dev/null << 'EOF'
+sudo tee /usr/local/bin/start-vnc > /dev/null << 'EOF'
 #!/bin/bash
 
 sudo firewall-cmd --add-port=5900/tcp
@@ -15,7 +15,7 @@ swaymsg create_output HEADLESS-1
 wayvnc --output=HEADLESS-1 --max-fps=20 0.0.0.0 5900 &
 EOF
 
-sudo chmod +x /usr/local/bin/start-vnc.sh
+sudo chmod +x /usr/local/bin/start-vnc
 
 echo "done"
 
